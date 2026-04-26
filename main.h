c3ea4117a4bacf5e9b4c5b62ba8c1f5e9a4d2949
jfkul <joshua.kulik@icloud.com>
2026-04-26 20:56:00 +1000

    Render a triangle

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef OPENGL_ENGINE_MAIN_H
+#define OPENGL_ENGINE_MAIN_H
+
+#include <GLFW/glfw3.h>
+
+void processInput(GLFWwindow* window);
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+
+#endif //OPENGL_ENGINE_MAIN_H
